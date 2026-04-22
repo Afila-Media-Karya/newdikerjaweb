cd02ec394486db8eaf4cb72476f1c03000af95cf
zhorahmatt <info.rahmathidayat@gmail.com>
2026-04-22 10:55:25 +0800

    update queue table

--- config/queue.php
+++ config/queue.php
@@ -36,7 +36,7 @@ return [
 
         'database' => [
             'driver' => 'database',
-            'table' => 'jobs',
+            'table' => 'tb_export_kehadiran_jobs',
             'queue' => 'default',
             'retry_after' => 90,
             'after_commit' => false,
